62e0d2885bd66128e570fb8427aeab0d37d5a8fc
Giovanni Metitieri <GiovanniMet@users.noreply.github.com>
2019-02-27 13:58:54 +0100

    plesk-dkim-initialsetup.sh
    permette di creare la configurazione per il dominio che passiamo come parametro

---
+++ plesk-dkim-initialsetup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+NEW_DOMAIN_NAME=$1
+
+if [ $# -ne 1 ]; then
+        echo -e "ERROR! Give me a domain!"
+        echo -e "$0 domain.tld"
+        exit 1
+fi
+
+##path for temp file
+tmp_path=/tmp/dkim_tmp
+##temp filename prefix
+tmp_prefix=dkim_tmpfile_
+##temp file ending
+tmp_fe=.txt
+
+##path in customer www-root for dkim file
+dkim_rec_path=/dkim_keys
+##dkim filename prefix
+dkim_rec_prefix=dkim_key_
+##dkim file ending
+dkim_rec_fe=.txt
+
+##go to sleep, because plesk is not so fast...
+sleep 15
+
+##find customer www-root
+customer_root=$(/usr/local/psa/bin/domain --info ${NEW_DOMAIN_NAME} | grep "WWW-Root" | cut -d/ -f2-5)
+
+## create directories if they do not exist yet
+mkdir -p $tmp_path
+mkdir -p /$customer_root$dkim_rec_path
+
+## enable DKIM on the domain
+/usr/local/psa/bin/domain_pref --update ${NEW_DOMAIN_NAME} -sign_outgoing_mail true
+
+## recreate key
+openssl rsa -in /etc/domainkeys/${NEW_DOMAIN_NAME}/default -pubout -out $tmp_path/public_${NEW_DOMAIN_NAME}.key
+
+## crop first and last line of keyfile
+sed '1d;$d' $tmp_path/public_${NEW_DOMAIN_NAME}.key > $tmp_path/$tmp_prefix${NEW_DOMAIN_NAME}$tmp_fe
+rm $tmp_path/public_${NEW_DOMAIN_NAME}.key
+## remove newlines from the key
+dkim_key=$(tr -d '\n' < $tmp_path/$tmp_prefix${NEW_DOMAIN_NAME}$tmp_fe)
+rm $tmp_path/$tmp_prefix${NEW_DOMAIN_NAME}$tmp_fe
+
+## generate domainkey record and store to text file
+echo "default._domainkey.${NEW_DOMAIN_NAME} IN TXT \"v=DKIM1; k=rsa; p=$dkim_key" > /$customer_root$dkim_rec_path/$dkim_rec_prefix${NEW_DOMAIN_NAME}$dkim_rec_fe
